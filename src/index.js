bc5642accb5aa6c3b04d2b713393b0e6e936a292
nayokim <nayoung.kimpak@gmail.com>
2020-09-08 10:24:09 -0500

    hello component

--- src/index.js
+++ src/index.js
@@ -1,21 +1,49 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  console.log('Hello from component')
-
-  const now = new Date()
-  const a = 10 
-  const b = 20 
-
-  return(
-    <div> 
-      <p>Hello World , it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
+const Hello = () => {
+  return (
+    <div>
+      <p> Hello  World</p>
     </div>
   )
 }
+
+const App = () => {
+  return (
+    <div>
+      <h1> Greetings </h1>
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+// const App = () => {
+//   console.log('Hello from component')
+
+//   const now = new Date()
+//   const a = 10 
+//   const b = 20 
+
+//   return React.createElement(
+//     'div',
+//     null, 
+//     React.createElement(
+//       'p', null, 'Hello world, it is ', now.toString()
+//     ),
+//     React.createElement(
+//       'p' , null, a ,' plus ' , b, ' is ', a + b
+//     )
+//   )
+//     }
+    // <div> 
+    //   <p>Hello World , it is {now.toString()}</p>
+    //   <p>
+    //     {a} plus {b} is {a + b}
+    //   </p>
+    // </div>
 
 ReactDOM.render(<App/>, document.getElementById('root'))
