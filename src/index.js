b77f59e53eb1eda9d7d347e3347f2622a708b777
nayokim <nayoung.kimpak@gmail.com>
2020-09-10 14:06:41 -0500

    page re-rendering

--- src/index.js
+++ src/index.js
@@ -18,62 +18,62 @@ import ReactDOM from 'react-dom'
 // a => a + 100; 
 
 
-const t = [1,2,3]
-//map creates a new array which the function given as a parameter is used to create the items. 
-const m1 = t.map(value => value * 2)
-//function (value) {
-//   return value * 2; 
+// const t = [1,2,3]
+// //map creates a new array which the function given as a parameter is used to create the items. 
+// const m1 = t.map(value => value * 2)
+// //function (value) {
+// //   return value * 2; 
+// // }
+// // console.log(m1)
+
+// const m2 = t.map(value => '<li>' + value + '</li>')
+// // console.log(m2)
+
+
+// //object literals 
+
+// const object1 = {
+//   name:"arto hellas",
+//   age: 35, 
+//   education: 'phd'
 // }
-// console.log(m1)
 
-const m2 = t.map(value => '<li>' + value + '</li>')
-// console.log(m2)
+// const object2 = {
+//   name: 'full stack web application development', 
+//   level: 'intermediate studies',
+//   size: 5,
+// }
 
+// const object3 = {
+//   name: {
+//     first: 'dan', 
+//     last: 'abramov',
+//   }, 
+//   grades: [2,3,5,3],
+//   department: 'Stanford University',
+// }
 
-//object literals 
+// // console.log(object1.name)
+// // console.log(object2.name);
+// // console.log(object3.name);
 
-const object1 = {
-  name:"arto hellas",
-  age: 35, 
-  education: 'phd'
-}
+// const fieldName = 'age'
+// // console.log(object1[fieldName])
 
-const object2 = {
-  name: 'full stack web application development', 
-  level: 'intermediate studies',
-  size: 5,
-}
+// //object methods and this   - brief overview as newer versions of react use hooks where there is no need for defining objects with methods 
+// //we can assign methods to an object by defining properties that are functions 
 
-const object3 = {
-  name: {
-    first: 'dan', 
-    last: 'abramov',
-  }, 
-  grades: [2,3,5,3],
-  department: 'Stanford University',
-}
-
-// console.log(object1.name)
-// console.log(object2.name);
-// console.log(object3.name);
-
-const fieldName = 'age'
-// console.log(object1[fieldName])
-
-//object methods and this   - brief overview as newer versions of react use hooks where there is no need for defining objects with methods 
-//we can assign methods to an object by defining properties that are functions 
-
-const arto = {
-  name: 'Arto Hellas', 
-  age: 35, 
-  education: 'PhD',
-  greet: function(){
-    console.log('hello, my name is ' + this.name)
-  },
-  doAddition: function(a,b){
-    console.log(a + b)
-  }
-}
+// const arto = {
+//   name: 'Arto Hellas', 
+//   age: 35, 
+//   education: 'PhD',
+//   greet: function(){
+//     console.log('hello, my name is ' + this.name)
+//   },
+//   doAddition: function(a,b){
+//     console.log(a + b)
+//   }
+// }
 
 // arto.greet()
 // const referenceToGreet = arto.greet
@@ -93,66 +93,90 @@ const arto = {
 // referenceToAddition(10,15) // prints 25
 
 //methods can be assigned to objects even after the creation of the object 
-arto.growOlder = function(){
-  this.age +=1
-}
+// arto.growOlder = function(){
+//   this.age +=1
+// }
 
 // arto.greet()
 // arto.growOlder()
 // console.log(arto.age)
 
 // Classes 
-class Person{
-  constructor (name, age){
-    this.name = name
-    this.age = age
-  }
-  greet(){
-    console.log('hello, my name is ' + this.name)
-  }
-}
+// class Person{
+//   constructor (name, age){
+//     this.name = name
+//     this.age = age
+//   }
+//   greet(){
+//     console.log('hello, my name is ' + this.name)
+//   }
+// }
 
-const adam = new Person ('Adam Ondra', 35)
-// adam.greet()
+// const adam = new Person ('Adam Ondra', 35)
+// // adam.greet()
 
-const nayo = new Person ('Nayo', 30)
-// nayo.greet()
+// const nayo = new Person ('Nayo', 30)
+// // nayo.greet()
 
+//end js review 
 
-const Hello = (props) => {
-  return (
-    <div>
-      <p> Hello  {props.name}, you are {props.age} years old.</p>
-    </div>
+//The person's age does not have to be passed as a parameter to the function, since it can directly access all props that are passed to the component.
+// const Hello = ({name,age}) => {
+  //The props that are passed to the component are now directly destructured into the variables name and age.
+  // This means that instead of assigning the entire props object into a variable called props and then assigning its properties into the variables name and age
+  // we assign the values of the properties directly to variables by destructuring the props object that is passed to the component function as a parameter:
+  // const {name, age} = props
+  // const name = props.name
+  // const age = props.age
+
+//   const bornYear = () => new Date().getFullYear() - age;
+
+//   // const bornYear = () => {
+//   //   return new Date().getFullYear() - age;
+//   // };
+
+//   return (
+//     <div>
+//       <p>
+//         Hello {name}, you are {age} years old.
+//       </p>
+//       <p>So you were probably born in {bornYear()}</p>
+//     </div>
+//   );
+// }
+
+// const Footer = () => {
+//   return (
+//     <div>
+//       This is a footer
+//     </div>
+//   )
+// }
+
+// const App = (props) => {
+//   // const name = 'Peter'
+//   // const age = 10
+//   return (
+//     <div>
+   
+//       {/* <h1> Greetings </h1> */}
+//       {/* <Hello name = "George" age = {26+10} />
+//       <Hello name = "Daisy"  age = {10}/>
+//       <Hello name = {name} age = {age} />
+//       <Footer /> */}
+//     </div>
+//   )
+// }
+
+const App = (props) => {
+  const { counter } = props;
+  return (<div>{counter}</div>
   )
 }
 
-const Footer = () => {
-  return (
-    <div>
-      This is a footer
-    </div>
-  )
-}
+let counter = 1
 
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-
-  return (
-    <div>
-      <h1> Greetings </h1>
-      <Hello name = "George" age = {26+10} />
-      <Hello name = "Daisy"  age = {10}/>
-      <Hello name = {name} age = {age} />
-      <Footer />
-    </div>
-  )
-}
-
-
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App counter = {counter} />, document.getElementById('root'))
 // const App = () => {
 //   console.log('Hello from component')
 
@@ -181,4 +205,4 @@ ReactDOM.render(<App />, document.getElementById('root'))
 
 
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+// ReactDOM.render(<App/>, document.getElementById('root'))
